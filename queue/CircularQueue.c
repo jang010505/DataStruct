596e5a693a8dc035f2ea02853bfd7625fe0200d1
hg_yellow <jang010505@gmail.com>
2021-04-24 02:42:29 +0900

    creat : Queue

---
+++ queue/CircularQueue.c
@@ -0,0 +1,48 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+#define MAX_QUEUE_SIZE 100
+
+typedef int element;
+
+typedef struct {
+	element data[MAX_QUEUE_SIZE];
+	int front, rear;
+} QueueType;
+
+void error(char *message)
+{
+	fprintf(stderr, "%s\n", message);
+	exit(1);
+}
+
+void init_queue(QueueType *q)
+{
+	q->front = q->rear = 0;
+}
+
+int is_empty(QueueType *q)
+{
+	return (q->front == q->rear);
+}
+
+int is_full(QueueType *q)
+{
+	return ((q->rear + 1) % MAX_QUEUE_SIZE == q->front);
+}
+
+void enqueue(QueueType *q, element item)
+{
+	if (is_full(q))
+		error("큐가 포화상태입니다");
+	q->rear = (q->rear + 1) % MAX_QUEUE_SIZE;
+	q->data[q->rear] = item;
+}
+
+element dequeue(QueueType *q)
+{
+	if (is_empty(q))
+		error("큐가 공백상태입니다");
+	q->front = (q->front + 1) % MAX_QUEUE_SIZE;
+	return q->data[q->front];
+}
